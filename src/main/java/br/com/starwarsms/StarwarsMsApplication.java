7a6ed9c2e93b6a3db74e3c8987d9213e11f9d76c
Fabricio <fabricio.dvsc@outlook.com>
2025-08-14 19:13:03 -0300

    initial commit

---
+++ src/main/java/br/com/starwarsms/StarwarsMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.starwarsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarsMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarwarsMsApplication.class, args);
+    }
+
+}
